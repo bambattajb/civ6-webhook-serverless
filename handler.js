f60fb9cc78a1ee533cdfc430478e2a4f2e80ceb3
Joe Buckle <joe@white-fire.co.uk>
2020-05-14 10:54:17 +0100

    First commit

---
+++ handler.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const hook = process.env.DC_WEBHOOK;
+'use strict';
+
+module.exports.exec = async req => {
+
+    const xhr =
+        new XMLHttpRequest();
+    const body
+        = req.body;
+    const today = new Date();
+    const day =
+        String(today.getDate()).padStart(2, '0');
+    const month =
+        String(today.getMonth() + 1).padStart(2, '0');
+    const year =
+        today.getFullYear();
+    const hours =
+        today.getHours();
+    const minutes =
+        String((today.getMinutes() < 10 ? '0' : '') + today.getMinutes());
+
+    let content
+        = `Last turn completed on `;
+    content += `${day}-${month}-${year}`;
+    content += ` @ ${hours}:${minutes}\n`;
+    content += `\n`;
+    content += `Name: ${body.value1}\n`;
+    content += `Turn: ${body.value3}\n`;
+    content += `Player next up: ${body.value2}`;
+
+    xhr.
+        open("POST", hook);
+    xhr.
+        setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.
+        send(JSON.stringify({content})
+    );
+
+    return false;
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+##Civilization 6 Webhook to Discord
+A simple webhook handler for Civilization 6 designed to be hosted as a serverless function.
+
+### Usage
+1) Get Discord Webhook for channel to post to
+2) Create `.env` file and add url to `DC_WEBHOOK` 
+3) Deploy `sls deploy`
